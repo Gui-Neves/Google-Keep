3a67ce44f9ddc360553398ecffc919839ad03074
Guilherme Neves <guilhermevdneves@gmail.com>
2023-10-29 15:42:35 -0300

    Adds responsivity

--- app/view/NoteView.js
+++ app/view/NoteView.js
@@ -13,14 +13,15 @@ class NoteView {
                 <div class="notesView" >
                     <textarea rows="1" class="inputTitle note-title-${element.key}" id="title">${element.note._noteTitle}</textarea>
                     <textarea rows="6" class="inputContent note-content-${element.key}" id="content">${element.note._noteContent}</textarea>
-                    <button class="noteButtonEdit" onclick="noteController._editNote(${element.key})">Edit</button>
-                    <button class="noteButtonRemove" onclick="noteController._removeNote(${element.key})">Delete</button>
+                    <div class="buttonsContainer">
+                        <button class="noteButtonEdit" onclick="noteController._editNote(${element.key})">Edit</button>
+                        <button class="noteButtonRemove" onclick="noteController._removeNote(${element.key})">Delete</button>
+                    </div>
                 </div>
             `}).join(``);
 
     }
     update(list) {
- 
         this._element.innerHTML = this._template(list);
     }
 }
